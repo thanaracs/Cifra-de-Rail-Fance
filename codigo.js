e54eaf210cd617b850a598930e26bf02acfbd5ec
Thainara <thai.cds1@gmail.com>
2022-05-09 17:24:07 -0400

    Finalizado jogando dados no campo texto. Faltando Descifrar

--- codigo.js
+++ codigo.js
@@ -1,20 +1,21 @@
 function cifrador() {
     var linhasArray = [];
-    var arrayDeLinhas = [];
     var chave = document.getElementById('chave').value;
     var palavra = document.getElementById('palavra').value;
     var linhaAtual = chave;
     var sinalAtual = '+';
+    var resultadoFinal = '';
 
     if (palavra.length < chave) {
         window.alert('Informe uma palavra maior');
     }
-
     //iniciar o array de linhas
     for (let i = 0; i < chave; i++) {
         linhasArray.push([]);
     }
+
     let contador = 0;
+
     for (let coluna = 0; coluna < palavra.length; coluna++) {
 
         for (let linha = 0; linha < chave.length; linha++) {
@@ -42,10 +43,28 @@ function cifrador() {
                 console.log("Opa, final da palavra")
             }
         }
-
-
     }
-
+    console.log(linhasArray)
+    for (let i = 0; i < linhasArray.length; i++) {
+        console.log(linhasArray[i])
+        for (let j = 0; j < linhasArray[i].length; j++) {
+            if (linhasArray[i][j] != undefined) {
+                resultadoFinal = resultadoFinal + linhasArray[i][j];
+            }
+        }
+    }
+    console.log(resultadoFinal)
+    document.getElementById('respostaCifrada').value = resultadoFinal;
 }
+/*
+t     r
+ h   a a
+  a n
+   i
+*/
 
 //DESCRIPTOGRAFAR
+function descifrador() {
+    var chave = document.getElementById('chave').value;
+    var palavra = document.getElementById('palavra').value;
+}
