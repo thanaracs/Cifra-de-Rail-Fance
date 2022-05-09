a677398240c7e175baaf33029b90896ed9e41ad4
Thainara <thai.cds1@gmail.com>
2022-05-09 16:53:47 -0400

    Criptografia finalizada!

--- codigo.js
+++ codigo.js
@@ -13,68 +13,39 @@ function cifrador() {
     //iniciar o array de linhas
     for (let i = 0; i < chave; i++) {
         linhasArray.push([]);
-
     }
     let contador = 0;
-    for (let linha = 0; linha < palavra.length; linha++) {
-        for (let coluna = 0; coluna < chave.length; coluna++) {
-            linhasArray[contador][contador] = palavra[linha][coluna];
-            contador = contador + 1
-            linhaAtual = linhaAtual - 1;
+    for (let coluna = 0; coluna < palavra.length; coluna++) {
 
-            if (contador == chave) {
-                if (linhaAtual != 0) {
-                    linhaAtual = linhaAtual - 1;
-                    linhasArray[linhaAtual][contador] = palavra
-                }
+        for (let linha = 0; linha < chave.length; linha++) {
+            console.log(palavra, palavra[coluna])
+            console.log(linhasArray, coluna, contador)
+            linhasArray[contador][coluna] = palavra[coluna];
+
+            if (sinalAtual == '+') {
+                contador = contador + 1;
+            }
+            if (sinalAtual == '-') {
+                contador = contador - 1;
             }
 
-            if (contador == chave) {
-                linhaAtual = linhaAtual - 1;
+            if ((contador + 1) >= chave) {
+                sinalAtual = '-'
+                console.log('entrei, teste')
             }
 
-
-
-
-            // if ((linhaAtual + 1) >= chave) {
-
-            //     linhaAtual = linhaAtual - 1;
-            // }
-            console.log(linhasArray)
-            console.log(linhaAtual)
+            if ((contador - 1) < 0) {
+                sinalAtual = '+'
+            }
 
             if ((linha + 1) > palavra.length) {
                 console.log("Opa, final da palavra")
             }
         }
+
+
     }
 
 }
 
-
-/*
-        for (let i = 0; i < palavra.length; i++) {
-            linha = linha + ' ';
-            console.log(linha);
-            //i + 1 == palavra.length && (palavra.length % 2 != 0))
-            //condição da ultima letra
-            if ((i + 1) >= palavra.length) {
-                console.log("Opa, final da palavra")
-            }
-        }
-
-
-        railFance.push(linha);
-        console.log(railFance);
-        for (let i = 0; i < chave; i++) {
-
-
-            console.log('opa,entrei')
-            arrayDeLinhas.push(railFance);
-
-            if ()
-        }
-        //array de lihas contem o tamanho da array + a palvra em cada uma das linhas
-        console.log(arrayDeLinhas); *
-        /
-*/
+//DESCRIPTOGRAFAR
